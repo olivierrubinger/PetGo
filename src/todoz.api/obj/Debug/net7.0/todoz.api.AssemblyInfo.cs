f7ea811107d1780cd1913f4657cee917afdb1e5b
gomesluiz <gomes.luiz@gmail.com>
2024-08-12 18:11:55 -0300

    Update documentation.

--- src/todoz.api/obj/Debug/net7.0/todoz.api.AssemblyInfo.cs
+++ src/todoz.api/obj/Debug/net7.0/todoz.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todoz.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0724429a20a7af5338e268a52b1113a4f67b1b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1fe2ee1c43d5628be7f7384ae5de6ee68da61c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("todoz.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todoz.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
