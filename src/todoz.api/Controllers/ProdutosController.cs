0461a75a38942144822c78b0ec5adf2b60037354
marcelloabritta <abrittamarcello@gmail.com>
2025-09-25 13:16:37 -0300

    Criação do Controller Produto

---
+++ src/todoz.api/Controllers/ProdutosController.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using todoz.api.Data;
+using todoz.api.Models;
+
+namespace todoz.api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetProdutos()
+        {
+            var produtos = await _context.Produtos.ToListAsync();
+            return Ok(produtos);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Produto produto)
+        {
+            _context.Produtos.Add(produto);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetById", new { id = produto.Id }, produto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+            return produto == null ? NotFound() : Ok(produto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, Produto produto)
+        {
+            if (id != produto.Id) return BadRequest();
+            var produtoDb = await _context.Produtos.AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (produtoDb == null) return NotFound();
+
+            _context.Produtos.Update(produto);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+
+            if (produto == null) return NotFound();
+
+            _context.Produtos.Remove(produto);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+}
